030e8f9ef256c1a8229dedc8f9b070a49f80c3d3
Dee Race <deerace12@gmail.com>
2018-06-14 13:52:46 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,11 @@
 class SchoolClassesController < ApplicationController
 
+  def create 
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_path(@school_class)
+  end
+
+
+
 end
